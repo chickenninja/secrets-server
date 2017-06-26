87544f9b0635a306a7756ea384346464c4a685bc
Sean <sean@example.com>
2017-06-26 23:21:33 +0100

    added eslint

--- secrets-server.js
+++ secrets-server.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 app.get('/ping', function (req, res) {
-  res.status(200).send()
+	res.status(200).send()
 })
 
 app.get('/*', function (req, res) {
-  fs.readFile('./secrets/' + req.params[0], function (err, data) {
-	if(err) {
-		return res.status(404).json({success:false, error:"secret not found"})
-    }
-    return res.status(200).set({'content-type':'text/plain'}).send(data)
-  })
+	fs.readFile('./secrets/' + req.params[0], function (err, data) {
+		if(err) {
+			return res.status(404).json({success:false, error:'secret not found'})
+		}
+		return res.status(200).set({'content-type':'text/plain'}).send(data)
+	})
 
 })
 
